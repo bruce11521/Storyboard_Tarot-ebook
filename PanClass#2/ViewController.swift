3307ebd8da663f5f7d56d1fcf6a6b933498c1c89
bruce11521 <bruce11521@gmail.com>
2019-11-05 19:46:38 +0800

    Initial Commit

---
+++ PanClass#2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PanClass#2
+//
+//  Created by 林伯翰 on 2019/11/5.
+//  Copyright © 2019 Bruce Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
